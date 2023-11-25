b713dd4d23b114721a4dc597c5449af7e4305d70
hahrahr <hammedsaudix@gmail.com>
2023-11-26 01:54:56 +0200

    Create ItroActivity

--- app/src/main/java/com/projecttask/memoralife/ui/SplashActivity.java
+++ app/src/main/java/com/projecttask/memoralife/ui/SplashActivity.java
@@ -3,7 +3,9 @@ package com.projecttask.memoralife.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.projecttask.memoralife.R;
 
@@ -14,5 +16,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+            startActivity(intent);
+            finish();
+
+        }, 3000);
+
+
     }
 }
